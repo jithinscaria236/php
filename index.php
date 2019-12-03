9ab932637bbf4d91c8e699a497a94e839adef2da
jithinscaria236 <jithinscaria236@gmail.com>
2019-12-03 13:13:41 +0530

    Modified

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <html>
-<body bgcolor="cyan">
+<body bgcolor="yellow">
 <?php
 echo "HI JITHIN! Welcome to OPENSHIFT Deployment"
 ?>
+<p>
 <img src="https://media.licdn.com/dms/image/C4E03AQFyWQPqLsxdOg/profile-displayphoto-shrink_200_200/0?e=1580947200&v=beta&t=HiNtDJyJFtjxRYblenjYOnbzo09PeFN0ns2b-E-AHyA" alt="Smiley face" height="200" width="200"> 
+</p>
+<p>
+  <img src="https://media.licdn.com/dms/image/C5103AQG3yXDOmFBUkA/profile-displayphoto-shrink_200_200/0?e=1580947200&v=beta&t=e3HiYvjW7eyjpIMsedN6HI3T_nPB3gAN4jn3sMWgkuM" height="200" width="200"> 
+
+ </p>
 </body>
 </html>
 
