870479609e384f422f0b6cb7bdf12c750c28d55e
jithinscaria236 <jithinscaria236@gmail.com>
2019-12-03 13:01:04 +0530

    edit

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php
 echo "HI JITHIN! Welcome to OPENSHIFT Deployment"
 ?>
+<img src="https://media.licdn.com/dms/image/C4E03AQFyWQPqLsxdOg/profile-displayphoto-shrink_200_200/0?e=1580947200&v=beta&t=HiNtDJyJFtjxRYblenjYOnbzo09PeFN0ns2b-E-AHyA" alt="Smiley face" height="42" width="42"> 
 </body>
 </html>
 
